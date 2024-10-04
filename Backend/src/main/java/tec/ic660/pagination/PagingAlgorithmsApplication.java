1dfda62fdcfa4d55ae8b49a42fd32f838576f508
BrandonRetana <branrecha140356@gmail.com>
2024-10-04 00:03:14 -0600

    initial commit

---
+++ Backend/src/main/java/tec/ic660/pagination/PagingAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package tec.ic660.pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagingAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagingAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/tec/ic660/pagination/PagingAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package tec.ic660.pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagingAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
