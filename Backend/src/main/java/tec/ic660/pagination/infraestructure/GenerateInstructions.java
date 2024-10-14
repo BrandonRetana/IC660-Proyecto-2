c3b26af796235ba2f7f39a13628df235e6f4d2a4
BrandonRetana <branrecha140356@gmail.com>
2024-10-13 18:55:10 -0600

    feat: add generate instructions functionality

--- Backend/src/main/java/tec/ic660/pagination/infraestructure/GenerateInstructions.java
+++ Backend/src/main/java/tec/ic660/pagination/infraestructure/GenerateInstructions.java
@@ -1,6 +1,9 @@
 package tec.ic660.pagination.infraestructure;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Random;
@@ -21,10 +24,23 @@ public class GenerateInstructions {
     private static final int TYPE_DELETE = 3;
     private static final int TYPE_KILL = 4;
 
-    private String getStringInstruction(Integer id, Integer size) {
-        String result;
-
-        return null;
+    private String getStringInstruction(Integer id, Integer size, Integer typeOfString) {
+        String result = "";
+        switch (typeOfString) {
+            case TYPE_NEW:
+                result = "new("+id+","+size+")";
+                break;
+            case TYPE_USE:
+                result = "use("+id+")";
+                break;
+            case TYPE_DELETE:
+                result = "delete("+id+")";
+                break;
+            case TYPE_KILL:
+                result = "kill("+id+")";
+                break;
+        }
+        return result;
     }
 
     private int getRandomBetween(int min, int max) {
@@ -34,8 +50,13 @@ public class GenerateInstructions {
         return random.nextInt((max - min) + 1) + min;
     }
 
+    private void decrementValue(Integer pid) {
+        Integer value = this.process.get(pid);
+        this.process.put(pid, value - 1);
+    }
+
     private void generateProcess(Integer numberOfProcess, Integer numberOfInstructions) {
-        int remainingInstructions = numberOfInstructions; 
+        int remainingInstructions = numberOfInstructions;
 
         for (int i = 1; i <= numberOfProcess; i++) {
             int maxInstructions = remainingInstructions - (numberOfProcess - i);
@@ -54,46 +75,71 @@ public class GenerateInstructions {
         return lastInstruction.startsWith("kill");
     }
 
-    private String generateNewInstruction(int numberOfProcess) {
-        int pid = getRandomBetween(1, numberOfProcess);
+    private String generateNewInstruction(int numberOfProcess, int pid) {
         int size = getRandomBetween(MIN_PAGE_SIZE, MAX_PAGE_SIZE);
-        return getStringInstruction(pid, size);
+        Integer numberOfPTRs = this.process.get(pid);
+        this.processPTR.put(pid, numberOfPTRs + 1);
+        decrementValue(pid);
+        return getStringInstruction(pid, size, TYPE_NEW);
     }
 
-    public GenerateInstructions() {
-        this.seed = 10;
-
-        this.random = new Random(seed);
+    private String generateUseInstruction(Integer pid) {
+        Integer ptr = getRandomBetween(1, this.processPTR.get(pid));
+        decrementValue(pid);
+        return getStringInstruction(ptr, ptr, TYPE_USE);
     }
 
-    /*TODO: Aqui la logica deberia cambiar, generar el pid al que se le va a agregar la intruccion, agregar la verfiicacion de si este proceso existe para ello 
-    se deben aplanar los keys del map de procesos, y generar un random para en entre esa lista, luego verificar que a ese pid le queden instrucciones, y en el mapa de lls procesos 
-    con los PTR verificar que tenga al menos uno, para las instrucciones, use o delete. 
-    */  
+    private String generateDeleteInstruction(Integer pid) {
+        Integer ptr = this.processPTR.get(pid);
+        Integer numberOfPtrs = this.process.get(pid);
+        this.process.put(pid, numberOfPtrs - 1);
+        decrementValue(pid);
+        return getStringInstruction(ptr, ptr, TYPE_DELETE);
+    }
+
+    private String generateKillInstruction(Integer pid) {
+        return getStringInstruction(pid, pid, TYPE_KILL);
+    }
+
     public Queue<String> getInstructions(Integer numberOfInstructions, Integer numberOfProcess) {
         int remainingInstructions = numberOfInstructions;
         Queue<String> instructions = new LinkedList<>();
 
         generateProcess(numberOfProcess, numberOfInstructions);
+        List<Integer> keys = new LinkedList<>(this.process.keySet());
 
         while (remainingInstructions > 0) {
-            int typeOfInstruction = getRandomBetween(1, 4);
+            int randomIndex = getRandomBetween(0, keys.size());
+            int typeOfInstruction = getRandomBetween(1, 3);
+
+            if (this.process.get(randomIndex) == 1) {
+                instructions.add(generateKillInstruction(numberOfProcess));
+                keys.remove(randomIndex);
+                remainingInstructions--;
+                continue;
+            }
 
             if (typeOfInstruction == TYPE_NEW && !isLastInstructionKill(instructions)) {
-                instructions.add(generateNewInstruction(numberOfProcess));
+                instructions.add(generateNewInstruction(numberOfProcess, randomIndex));
                 remainingInstructions--;
             }
 
             if (typeOfInstruction == TYPE_USE) {
-                
+                Integer numberOfPTRs = processPTR.get(randomIndex);
+                if (numberOfPTRs < 1) {
+                    continue;
+                }
+                instructions.add(generateUseInstruction(randomIndex));
+                remainingInstructions--;
             }
 
             if (typeOfInstruction == TYPE_DELETE) {
-                
-            }
-
-            if (typeOfInstruction == TYPE_KILL) {
-                
+                Integer numberOfPTRs = processPTR.get(randomIndex);
+                if (numberOfPTRs < 1) {
+                    continue;
+                }
+                instructions.add(generateDeleteInstruction(randomIndex));
+                remainingInstructions--;
             }
 
         }
@@ -101,7 +147,12 @@ public class GenerateInstructions {
         return instructions;
     }
 
-    
+    public GenerateInstructions() {
+        this.seed = 10;
+        this.process = new HashMap<>();
+        this.processPTR = new HashMap<>();
+        this.random = new Random(seed);
+    }
 
     public Integer getSeed() {
         return seed;
