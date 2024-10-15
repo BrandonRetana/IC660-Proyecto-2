67a68971345381e247aad9adca10ab87d4c0443a
BrandonRetana <branrecha140356@gmail.com>
2024-10-14 21:37:45 -0600

    fix: disable credentials in cors

--- Backend/src/main/java/tec/ic660/pagination/infraestructure/config/CorsConfig.java
+++ Backend/src/main/java/tec/ic660/pagination/infraestructure/config/CorsConfig.java
@@ -4,16 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite CORS en todas las rutas
-                .allowedOrigins("http://localhost:5173") // Permite todas las fuentes. Puedes cambiar "*" por dominios
+                .allowedOrigins("*") // Permite todas las fuentes. Puedes cambiar "*" por dominios específicos.
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permite los métodos HTTP permitidos
                 .allowedHeaders("*") // Permite todos los headers
-                .allowCredentials(true); // Si necesitas enviar cookies o credenciales de autenticación
+                .allowCredentials(false); // Si necesitas enviar cookies o credenciales de autenticación
     }
 }
