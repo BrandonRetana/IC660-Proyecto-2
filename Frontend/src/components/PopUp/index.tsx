d4b85fdad3e5760e517f959be08042e0d8904a46
Ervin Rodríguez <ervinrodriguez140@gmail.com>
2024-10-17 21:44:03 -0600

    dropdown

--- Frontend/src/components/PopUp/index.tsx
+++ Frontend/src/components/PopUp/index.tsx
@@ -181,21 +181,29 @@ function PopUp({
                 <div className="HStack optionsContainer">
                   <div className="inputContainer">
                     <label>Procesos:</label>
-                    <input
+                    <select
                       name="process"
-                      type="number"
                       value={process || ""}
                       onChange={(e) => setProcess(Number(e.target.value))}
-                    />
+                    >
+                      <option value=""></option>
+                      <option value={10}>10</option>
+                      <option value={50}>50</option>
+                      <option value={100}>100</option>
+                    </select>
                   </div>
                   <div className="inputContainer">
                     <label>Operaciones:</label>
-                    <input
+                    <select
                       name="operations"
-                      type="number"
                       value={operations || ""}
                       onChange={(e) => setOperations(Number(e.target.value))}
-                    />
+                    >
+                      <option value=""></option>
+                      <option value={500}>500</option>
+                      <option value={1000}>1000</option>
+                      <option value={5000}>5000</option>
+                    </select>
                   </div>
                 </div>
               )}
